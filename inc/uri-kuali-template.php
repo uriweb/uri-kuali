2a522741cdcc7fe1617377faa5d4b3815b4c2aa5
alexandragauss <alexandra_gauss@uri.edu>
2022-11-18 12:11:47 -0500

    Add teaching method to course description output

--- inc/uri-kuali-template.php
+++ inc/uri-kuali-template.php
@@ -16,6 +16,6 @@
   <div class="uri-kuali-course-header">
     <span class="uri-kuali-course-code"><?php print $attributes['subject'] . ' ' . $course->number; ?></span>
     <h3 class="uri-kuali-course-title"><?php print $course->title; ?></h3>
-  </div>
-  <p class="uri-kuali-course-description"><?php print $course->description; ?></p>
+    </div>
+  <p class="uri-kuali-course-description"><?php print $course->description . ' Teaching Method: ' . $course->teachingMethod; ?></p>
 </div>
